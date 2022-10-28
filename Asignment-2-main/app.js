0f1678a91dfacb33db08dac6ec28a9275503d253
ericaslaugher <113582580+ericaslaugher@users.noreply.github.com>
2022-10-28 17:05:44 +0700

    bla

--- Asignment-2-main/app.js
+++ Asignment-2-main/app.js
@@ -11,7 +11,7 @@ var apiRouter = require('./routes/api')
 
 var mongoose = require('mongoose')
 var url =
-    'mongodb+srv:mongodb+srv:minhbeo223@cluster0.u9ccc9w.mongodb.net/App'
+    'mongodb+srv:mongodb+srv:minhbeo223@cluster0.u9ccc9w.mongodb.net/app'
 
 mongoose.connect(url, { useNewUrlParser: true }, err => {
     if (!err) {
