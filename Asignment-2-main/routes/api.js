966fa3d2fc9c98adf73cfdcc491fb00110a950ec
ericaslaugher <113582580+ericaslaugher@users.noreply.github.com>
2022-10-28 17:30:09 +0700

    bla

--- Asignment-2-main/routes/api.js
+++ Asignment-2-main/routes/api.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const StudentModel = require('../models/ProductModel')
+const ProductModel = require('../models/ProductModel')
 const router = express.Router()
 
 //view all: select * from student
 router.get('/', (req, res) => {
-   StudentModel.find((err, data) => {
+   ProductModel.find((err, data) => {
       if (!err) {
          res.json(data)
       }
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 
 //view by id : select * from student where _id = 'id'
 router.get('/:id', (req, res) => {
-   StudentModel.findById(req.params.id, (err, data) => {
+   ProductModel.findById(req.params.id, (err, data) => {
       if (!err) {
          res.json(data)
       }
@@ -22,17 +22,17 @@ router.get('/:id', (req, res) => {
 
 //delete: delete from student where _id = 'id'
 router.delete('/:id', (req, res) => {
-   StudentModel.findByIdAndDelete(req.params.id, (err) => {
+   ProductModel.findByIdAndDelete(req.params.id, (err) => {
       if (!err) {
          //res.send("Delete student succeed !")
-         res.json({ "message": "delete student succeed" })
+         res.json({ "message": "delete product succeed" })
       }
    })
 })
 
 //add: insert (...) values (...) into student 
 router.post('/', (req, res) => {
-   StudentModel.create(req.body, (err, data) => {
+   ProductModel.create(req.body, (err, data) => {
       if (!err) {
          res.json(data)
       }
@@ -41,7 +41,7 @@ router.post('/', (req, res) => {
 
 //update: update from student where _id = "id"
 router.put('/:id', (req, res) => {
-   StudentModel.findByIdAndUpdate(req.params.id, req.body,
+   ProductModel.findByIdAndUpdate(req.params.id, req.body,
       (err, data) => {
          if (!err) {
             res.json(data)
